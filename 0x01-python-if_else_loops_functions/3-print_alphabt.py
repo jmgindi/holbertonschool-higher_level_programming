41b2cdd69334b0bb59759ceedd014197d4d263ee
jmgindi <jmgindi@gmail.com>
2018-12-04 23:05:39 +0000

    fixed spacing issue

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for a in range(ord('a'), (ord('z') + 1)):
     if a not in [ord('e'), ord('q')]:
-        print("{}".format(chr(a)), end = "")
+        print("{}".format(chr(a)), end="")
