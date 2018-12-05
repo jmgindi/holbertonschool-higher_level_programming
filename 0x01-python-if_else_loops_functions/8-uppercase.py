e13d235d41e3a50e5851f6df2a4b7654d348e9e7
jmgindi <jmgindi@gmail.com>
2018-12-05 00:14:25 +0000

    first upload 8, pep8 compliant

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            c = chr(ord(c) - 32)
+        else:
+            c = c
+
+        print("{}".format(c), end="")
+    print("")
