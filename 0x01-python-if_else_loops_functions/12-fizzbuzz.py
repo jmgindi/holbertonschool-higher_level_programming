add7652bafe393e40c52f9b8aea3289fe136d0e7
jmgindi <jmgindi@gmail.com>
2018-12-05 01:09:40 +0000

    added 11, 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(101):
+        if i % 15 == 0:
+            n = "FizzBuzz"
+        elif i % 5 == 0:
+            n = "Buzz"
+        elif i % 3 == 0:
+            n = "Fizz"
+        else:
+            n = i
+        print("{} ".format(n), end="")
