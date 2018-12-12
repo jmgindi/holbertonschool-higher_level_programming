d6cf286cc79765af8ab576811de91d9e61748c2b
jmgindi <jmgindi@gmail.com>
2018-12-12 01:56:19 +0000

    first upload of locally tested python files

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    lm = len(matrix[0])
+    for row in range(len(matrix)):
+        for i in range(lm):
+            if i < lm - 1:
+                print("{:d}".format(matrix[row][i]), end=" ")
+            else:
+                print("{:d}".format(matrix[row][i]), end="")
+        print("")
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    a = my_list[0]
+    for i in range(len(my_list)):
+        if a < my_list[i]:
+            a = my_list[i]
+    return a
