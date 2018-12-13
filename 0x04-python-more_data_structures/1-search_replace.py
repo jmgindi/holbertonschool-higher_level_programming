585139c976a5ae07a5f5c8eecc43c335b66c9ff6
jmgindi <jmgindi@gmail.com>
2018-12-13 21:14:17 +0000

    removed extraneous enumerate

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    for index, element in enumerate(my_list):
+    for element in my_list:
         if element == search:
             new_list.append(replace)
         else:
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,7 +4,7 @@ def uniq_add(my_list=[]):
     sum = 0
     for i in my_list:
         if i not in uniqs:
-           sum += i
-           uniqs.append(i)
+            sum += i
+            uniqs.append(i)
 
     return sum
