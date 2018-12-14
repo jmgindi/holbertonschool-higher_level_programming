f0eaa19721ee9431a575944b4c09459d4f71e1e6
jmgindi <jmgindi@gmail.com>
2018-12-14 02:28:18 +0000

    fixed pep8 and updated code for pythonicism

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str:
+    if type(roman_string) is not str:
         return None
     answer = 0
-    convert = { 'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                'C': 100, 'D': 500, 'M': 1000 }
+    convert = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+               'C': 100, 'D': 500, 'M': 1000}
     for idx, c in enumerate(roman_string):
         if c not in convert:
             return None
-        if (idx + 1) == len(roman_string) or convert[c] >= convert[roman_string[idx+1]]:
+        if (idx + 1) == len(roman_string) or \
+           convert[c] >= convert[roman_string[idx+1]]:
             answer += convert[c]
         else:
             answer -= convert[c]
