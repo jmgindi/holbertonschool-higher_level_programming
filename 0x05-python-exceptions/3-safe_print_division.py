a62c4376a1d1111337386d6165899d3472501b60
jmgindi <jmgindi@gmail.com>
2019-01-02 23:09:10 +0000

    uncapitalized 'r'

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         div = None
     finally:
-        print("Inside Result: {}".format(div))
+        print("Inside result: {}".format(div))
     return div
