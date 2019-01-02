7c9c0af07f1dbcb28cb806cac9e387c355ca8d2a
jmgindi <jmgindi@gmail.com>
2019-01-02 23:26:39 +0000

    added extra exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,4 +7,7 @@ def safe_print_list(my_list=[], x=0):
             i -= 1
             break
     print("")
-    return i + 1
+    try:
+        return i + 1
+    except UnboundLocalError:
+        return 0
