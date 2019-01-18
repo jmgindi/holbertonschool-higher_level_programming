289ccce3e3c620fc8f4f8c9139b0903fb52828bf
jmgindi <jmgindi@gmail.com>
2019-01-18 03:33:23 +0000

    fixed pep8 issue

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -4,6 +4,7 @@ module contains 1 function:
     read_lines
 """
 
+
 def read_lines(filename="", nb_lines=0):
     """ read_lines prints nb_lines lines of a file
     to stdout
@@ -11,7 +12,7 @@ def read_lines(filename="", nb_lines=0):
     with open(filename) as f:
         i = len(list(f))
         if nb_lines >= i or nb_lines <= 0:
-            nb_lines = i;
+            nb_lines = i
         f.seek(0, 0)
         for count in range(nb_lines):
             print(f.readline(), end="")
