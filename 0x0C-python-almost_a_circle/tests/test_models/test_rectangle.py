0ab3d6e3f7758b5037ce98ef2043efd19d3b8be5
jmgindi <jmgindi@gmail.com>
2019-01-28 05:52:08 +0000

    added docstrings to all tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -12,76 +12,112 @@ unittest module for rectangle class
 """
 
 class TestIntValidator(unittest.TestCase):
-    """
+    """ TestCase for checking the int validator
     """
     def setUp(self):
+        """ setUp function, resets nb_objects
+        and creates a rectangle
+        """
         Base.nbreset()
         self.rect = Rectangle(2, 2, 2, 2)
 
     def testStdUsage(self):
+        """ standard usage
+        """
         self.rect.width = 4
         self.assertEqual(self.rect.width, 4)
 
     def testNegID(self):
+        """ negative as ID
+        """
         self.rect.id = -5
         self.assertEqual(self.rect.id, -5)
 
     def testValidateType(self):
+        """ tries to validate a float
+        """
         with self.assertRaises(TypeError):
             self.rect.int_validator("x", 3.719)
 
     def testValidateTypeWidth(self):
+        """ tries to validate a string as width
+        """
         with self.assertRaises(TypeError):
             self.rect.width = "seven"
 
     def testValidateTypeHeight(self):
+        """ tries to validate a list as height
+        """
         with self.assertRaises(TypeError):
             self.rect.height = [1, 2, 3]
 
     def testValidateTypeX(self):
+        """ tries to validate a dict as x
+        """
         with self.assertRaises(TypeError):
             self.rect.x = {'key': 'value'}
 
     def testValidateTypeY(self):
+        """ tries to validate a single character as y
+        """
         with self.assertRaises(TypeError):
             self.rect.y = 'c'
 
     def testValidateNegWidth(self):
+        """ tries a negative as width
+        """
         with self.assertRaises(ValueError):
             self.rect.width = -50
 
     def testValidateNegHeight(self):
+        """ negative height
+        """
         with self.assertRaises(ValueError):
             self.rect.height = -5
 
     def testValidateZeroWidth(self):
+        """ 0 as width
+        """
         with self.assertRaises(ValueError):
             self.rect.width = 0
 
     def testValidateZeroHeight(self):
+        """ 0 as height
+        """
         with self.assertRaises(ValueError):
             self.rect.height = 0
 
     def testValidateZeroX(self):
+        """ 0 as x
+        """
         self.rect.x = 0
         self.assertEqual(self.rect.x, 0)
 
     def testValidateNegX(self):
+        """ negative as x
+        """
         with self.assertRaises(ValueError):
             self.rect.x = -15
 
     def testValidateZeroY(self):
+        """ 0 as y
+        """
         self.rect.y = 0
         self.assertEqual(self.rect.y, 0)
 
     def testValidateNegY(self):
+        """ negative as y
+        """
         with self.assertRaises(ValueError):
             self.rect.y = -1
 
 class TestRectangle(unittest.TestCase):
-    """
+    """ Rectangle TestCase for other functions
     """
     def setUp(self):
+        """ setUp deletes Rectangle.json if it exists,
+        resets nb_objects to 0, and creates a new rectangle
+        """
         try:
             os.remove("Rectangle.json")
         except FileNotFoundError:
@@ -90,61 +126,93 @@ class TestRectangle(unittest.TestCase):
         self.rect = Rectangle(5, 7, 1, 3)
 
     def testBadInit(self):
+        """ tests an init with no args
+        """
         with self.assertRaises(TypeError):
             r = Rectangle()
 
     def testBadInitOneArg(self):
+        """ only 1 arg
+        """
         with self.assertRaises(TypeError):
             r = Rectangle(1)
 
     def testTooManyArgs(self):
+        """ too many args to init
+        """
         with self.assertRaises(TypeError):
             r = Rectangle(6, 6, 6, 6, 6, 6)
 
     def testSubclass(self):
+        """ tests if Rectangle is a subclass of Base
+        """
         self.assertTrue(issubclass(type(self.rect), Base))
 
     def testWidth(self):
+        """ tests width
+        """
         self.assertEqual(self.rect.width, 5)
 
     def testWidthSetter(self):
+       """ tests the width setter
+       """
         self.rect.width = 2
         self.assertEqual(self.rect.width, 2)
 
     def testHeight(self):
+        """ tests height
+        """
         self.assertEqual(self.rect.height, 7)
 
     def testHeightSetter(self):
+        """ tests height setter
+        """
         self.rect.height = 3
         self.assertEqual(self.rect.height, 3)
 
     def testX(self):
+        """ tests x
+        """
         self.assertEqual(self.rect.x, 1)
 
     def testXSetter(self):
+        """ tests x setter
+        """
         self.rect.x = 4
         self.assertEqual(self.rect.x, 4)
 
     def testY(self):
+        """ tests y
+        """
         self.assertEqual(self.rect.y, 3)
 
     def testYSetter(self):
+        """ tests y setter
+        """
         self.rect.y = 5
         self.assertEqual(self.rect.y, 5)
 
     def testID(self):
+        """ tests id
+        """
         self.assertEqual(self.rect.id, 1)
 
     def testArea(self):
+        """ tests area
+        """
         self.assertEqual(self.rect.area(), 35)
 
     def testStr(self):
+        """ tests __str__
+        """
         s = str(self.rect)
         self.assertEqual(
             s, '[Rectangle] (1) 1/3 - 5/7'
         )
 
     def testDisplayNoOffset(self):
+        """ tests display with no offset
+        """
         r = Rectangle(2, 2, 0, 0)
         with io.StringIO() as f, redirect_stdout(f):
             r.display()
@@ -152,6 +220,8 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(s, "##\n##\n")
 
     def testDisplayWithX(self):
+        """ tests display with only x offset
+        """
         r = Rectangle(2, 2, 1, 0)
         with io.StringIO() as f, redirect_stdout(f):
             r.display()
@@ -159,6 +229,8 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(s, " ##\n ##\n")
 
     def testDisplayWithY(self):
+        """ tests display with only y offset
+        """
         r = Rectangle(2, 2, 0, 1)
         with io.StringIO() as f, redirect_stdout(f):
             r.display()
@@ -166,6 +238,8 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(s, "\n##\n##\n")
 
     def testDisplayWithXandY(self):
+        """ tests display with both offsets
+        """
         r = Rectangle(2, 2, 2, 2)
         with io.StringIO() as f, redirect_stdout(f):
             r.display()
@@ -173,6 +247,8 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(s, "\n\n  ##\n  ##\n")
 
     def testUpdateArgs(self):
+        """ updates args and tests them
+        """
         self.rect.update(1, 1, 1, 1, 1)
         self.assertEqual(self.rect.width, 1)
         self.assertEqual(self.rect.height, 1)
@@ -181,78 +257,112 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(self.rect.id, 1)
 
     def testUpdateBadArgs(self):
+        """ gives update a bad argument
+        """
         with self.assertRaises(TypeError):
             self.rect.update(1, "hello world")
 
     def testUpdateNoArgs(self):
+        """ tests update with no args
+        """
         r = Rectangle(5, 7, 1, 3, id=1)
         self.rect.update()
         self.assertEqual(r.to_dictionary(), self.rect.to_dictionary())
 
     @unittest.expectedFailure
     def testUpdateTooManyArgs(self):
+        """ tests that update with too many args does
+        not raise a TypeError
+        """
         with self.assertRaises(TypeError):
             self.rect.update(6, 6, 6, 6, 6, 6)
 
     def testUpdateKWargs(self):
+        """ tests update with keyworded args
+        """
         d1 = self.rect.to_dictionary()
         self.rect.update(id="cat", width=1, x=1)
         d2 = self.rect.to_dictionary()
         self.assertNotEqual(d1, d2)
 
     def testUpdateKWargsBadKey(self):
+        """ tests giving a bad keyword to update
+        """
         d1 = self.rect.to_dictionary()
         self.rect.update(boop=5)
         d2 = self.rect.to_dictionary()
         self.assertEqual(d1, d2)
 
     def testUpdateMixedArgs(self):
+        """ tests that args supersedes kwargs
+        """
         self.rect.update(3, id=4)
         self.assertEqual(self.rect.id, 3)
 
     def testRectToDict(self):
+        """ tests to_dictionary
+        """
         d = self.rect.to_dictionary()
         self.assertEqual(
             d, {'height': 7, 'id': 1, 'x': 1, 'width': 5, 'y': 3})
 
     def testRectToJSONType(self):
+        """ tests the type of to_json_string's return
+        """
         d = Rectangle.to_json_string([self.rect.to_dictionary()])
         self.assertIsInstance(d, str)
 
     def testRectJSONHasAttrs(self):
+        """ tests that to_json_string works properly
+        """
         d = Rectangle.to_json_string([self.rect.to_dictionary()])
         self.assertEqual(json.loads(d), [self.rect.to_dictionary()])
 
     def testRectEmptyToJSON(self):
+        """ tests an empty list into to_json_string
+        """
         d = Rectangle.to_json_string([])
         self.assertEqual(d, "[]")
 
     def testRectNonetoJSON(self):
+        """ tests None into to_json_string
+        """
         d = Rectangle.to_json_string(None)
         self.assertEqual(d, "[]")
 
     def testSaveRectToFile(self):
+        """ tests saving a rectangle to a file
+        """
         Rectangle.save_to_file([self.rect])
         with open("Rectangle.json") as f:
             self.assertEqual(
                 [self.rect.to_dictionary()], json.load(f))
 
     def testSaveRectsToFile(self):
+        """ tests saving multiple rectangles to a file
+        """
         r = Rectangle(2, 2, 2, 2)
         Rectangle.save_to_file([self.rect, r])
         with open("Rectangle.json") as f:
             self.assertEqual(len(json.load(f)), 2)
 
     def testSaveNothingToFile(self):
+        """ tests saving an empty list to a file
+        """
         Rectangle.save_to_file([])
         with open("Rectangle.json") as f:
             self.assertEqual(len(json.load(f)), 0)
 
     def testSaveBadFile(self):
+        """ tests that saving a non-serializable object
+        will raise an error
+        """
         with self.assertRaises(AttributeError):
             Rectangle.save_to_file("a string")
 
     def testFromJSON(self):
+        """ tests from_json_string
+        """
         d = Rectangle.from_json_string(
             '[{"id": 3, "width": 5, "height": 5, "x": 1, "y": 1}]'
         )
@@ -261,45 +371,64 @@ class TestRectangle(unittest.TestCase):
         )
 
     def testFromJSONNonString(self):
+        """ tests from_json_string with a bad
+        input
+        """
         with self.assertRaises(TypeError):
             d = Rectangle.from_json_string(TypeError)
 
     def testFromJSONBadString(self):
+        """ tests from_json_string with a non-serializable
+        """
         with self.assertRaises(ValueError):
             d = Rectangle.from_json_string("bad string")
 
     def testCreateRectangle(self):
+        """ tests create
+        """
         d = {"id": 1, "width": 2, "height": 2, "x": 2, "y": 2}
         r = Rectangle.create(**d)
         self.assertEqual(d, r.to_dictionary())
 
     def testCreateBadRectangle(self):
+        """ tests create with a bad dict
+        """
         d = {"id": 1, "width": 2, "height": 2, "x": 2, "y": -1}
         with self.assertRaises(ValueError):
             r = Rectangle.create(**d)
 
     def testCreateRectEmptyDict(self):
+        """ tests create on an empty dict
+        """
         d = {}
         r = Rectangle.create(**d)
         self.assertEqual(r.id, 2)
 
     def testLoadRectFromFile(self):
+        """ tests load_from_file
+        """
         Rectangle.save_to_file([self.rect])
         r = Rectangle.load_from_file()[0]
         self.assertEqual(
             self.rect.to_dictionary(), r.to_dictionary())
 
     def testLoadNothingFromFile(self):
+        """ tests loading an empty list from a file
+        """
         elist = []
         Rectangle.save_to_file(elist)
         r = Rectangle.load_from_file()
         self.assertEqual(elist, r)
 
     def testLoadRectNoFile(self):
+        """ tests loading from a non-existent file
+        """
         r = Rectangle.load_from_file()
         self.assertEqual(r, [])
 
     def testLoadBadRectangle(self):
+        """ tests loading a non-json object as a rectangle
+        """
         with open("Rectangle.json", "w") as f:
             f.write("Bad string")
         with self.assertRaises(ValueError):
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -13,9 +13,13 @@ unittest module for square class
 """
 
 class TestSquare(unittest.TestCase):
-    """
+    """ TestCase class for square class
     """
     def setUp(self):
+        """ setUp deletes Square.json if it exists
+        as well as creating a new square and
+        resetting nb_objects
+        """
         try:
             os.remove("Square.json")
         except FileNotFoundError:
@@ -24,54 +28,87 @@ class TestSquare(unittest.TestCase):
         self.sqr = Square(5, 7, 1)
 
     def testBadInit(self):
+        """ tests a zero argument init 
+        """
         with self.assertRaises(TypeError):
             r = Square()
 
     def testInitOneArg(self):
-            r = Square(1)
-            self.assertEqual(r.size, 1)
+        """ tests init with one argument
+        """
+        r = Square(1)
+        self.assertEqual(r.size, 1)
 
     def testTooManyArgs(self):
+        """ tests init with too many arguments
+        """
         with self.assertRaises(TypeError):
             r = Square(6, 6, 6, 6, 6, 6)
 
     def testSubclass(self):
+        """ tests if Square is a subclass of Base
+        """
         self.assertTrue(issubclass(type(self.sqr), Base))
 
+    def testSubRect(self):
+        """ tests if Square is a subclass of Rectangle
+        """
+        self.assertTrue(issubclass(type(self.sqr), Rectangle))
+
     def testSize(self):
+        """ tests size
+        """
         self.assertEqual(self.sqr.size, 5)
 
     def testSizeSetter(self):
+        """ tests size setter 
+        """
         self.sqr.size = 3
         self.assertEqual(self.sqr.size, 3)
 
     def testX(self):
+        """ tests x for square
+        """
         self.assertEqual(self.sqr.x, 7)
 
     def testXSetter(self):
+        """ tests x setter
+        """
         self.sqr.x = 4
         self.assertEqual(self.sqr.x, 4)
 
     def testY(self):
+        """ tests y
+        """
         self.assertEqual(self.sqr.y, 1)
 
     def testYSetter(self):
+        """ tests y setter
+        """
         self.sqr.y = 5
         self.assertEqual(self.sqr.y, 5)
 
     def testID(self):
+        """ tests id
+        """
         self.assertEqual(self.sqr.id, 1)
 
     def testArea(self):
+        """ tests area
+        """
         self.assertEqual(self.sqr.area(), 25)
 
     def testStr(self):
+        """ tests __str__
+        """
         s = str(self.sqr)
         self.assertEqual(
             s, '[Square] (1) 7/1 - 5'
         )
 
     def testDisplayNoOffset(self):
+        """ tests display with no offset
+        """
         r = Square(2)
         with io.StringIO() as f, redirect_stdout(f):
             r.display()
@@ -79,6 +116,8 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(s, "##\n##\n")
 
     def testDisplayWithX(self):
+        """ tests display with x offset
+        """
         r = Square(2, 1)
         with io.StringIO() as f, redirect_stdout(f):
             r.display()
@@ -86,6 +125,8 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(s, " ##\n ##\n")
 
     def testDisplayWithY(self):
+        """ tests display with y offset
+        """
         r = Square(2, 0, 1)
         with io.StringIO() as f, redirect_stdout(f):
             r.display()
@@ -93,6 +134,8 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(s, "\n##\n##\n")
 
     def testDisplayWithXandY(self):
+        """ tests display with x and y offset
+        """
         r = Square(2, 2, 2)
         with io.StringIO() as f, redirect_stdout(f):
             r.display()
@@ -100,6 +143,8 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(s, "\n\n  ##\n  ##\n")
 
     def testUpdateArgs(self):
+        """ tests update standard usage
+        """
         self.sqr.update(1, 1, 1, 1)
         self.assertEqual(self.sqr.size, 1)
         self.assertEqual(self.sqr.x, 1)
@@ -107,78 +152,113 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(self.sqr.id, 1)
 
     def testUpdateBadArgs(self):
+        """ tests update with a bad argument
+        """
         with self.assertRaises(TypeError):
             self.sqr.update(1, "hello world")
 
     def testUpdateNoArgs(self):
+        """ tests update with no arguments
+        """
         r = Square(5, 7, 1, 1)
         self.sqr.update()
         self.assertEqual(r.to_dictionary(), self.sqr.to_dictionary())
 
     @unittest.expectedFailure
     def testUpdateTooManyArgs(self):
+        """ tests that update with too many arguments
+        will not raise a TypeError
+        """
         with self.assertRaises(TypeError):
             self.sqr.update(6, 6, 6, 6, 6, 6)
 
     def testUpdateKWargs(self):
+        """ tests update with kwargs
+        """
         d1 = self.sqr.to_dictionary()
         self.sqr.update(id="cat", size=1, x=1)
         d2 = self.sqr.to_dictionary()
         self.assertNotEqual(d1, d2)
 
     def testUpdateKWargsBadKey(self):
+        """ tests update with a bad key
+        """
         d1 = self.sqr.to_dictionary()
         self.sqr.update(boop=5)
         d2 = self.sqr.to_dictionary()
         self.assertEqual(d1, d2)
 
     def testUpdateMixedArgs(self):
+        """ tests update with mixed arguments
+        """
         self.sqr.update(3, id=4)
         self.assertEqual(self.sqr.id, 3)
 
     def testSqrToDict(self):
+        """ tests square to dictionary
+        """
         d = self.sqr.to_dictionary()
         self.assertEqual(
             d, {'size': 5, 'id': 1, 'x': 7, 'y': 1})
 
     def testSqrToJSONType(self):
+        """ tests square to json's type
+        """
         d = Square.to_json_string([self.sqr.to_dictionary()])
         self.assertIsInstance(d, str)
 
     def testSqrJSONHasAttrs(self):
+        """ tests square to json retains attributes
+        """
         d = Square.to_json_string([self.sqr.to_dictionary()])
         self.assertEqual(json.loads(d), [self.sqr.to_dictionary()])
 
     def testSqrEmptyToJSON(self):
+        """ tests empty square to json
+        """
         d = Square.to_json_string([])
         self.assertEqual(d, "[]")
 
     def testSqrNonetoJSON(self):
+        """ tests an empty list to json string
+        """
         d = Square.to_json_string(None)
         self.assertEqual(d, "[]")
 
     def testSaveSqrToFile(self):
+        """ tests saving a square to a file
+        """
         Square.save_to_file([self.sqr])
         with open("Square.json") as f:
             self.assertEqual(
                 [self.sqr.to_dictionary()], json.load(f))
 
     def testSaveSqrsToFile(self):
+        """ tests saving multiple squares to
+        a file
+        """
         r = Square(2, 2, 2)
         Square.save_to_file([self.sqr, r])
         with open("Square.json") as f:
             self.assertEqual(len(json.load(f)), 2)
 
     def testSaveNothingToFile(self):
+        """ tests saving an empty list to a file
+        """
         Square.save_to_file([])
         with open("Square.json") as f:
             self.assertEqual(len(json.load(f)), 0)
 
     def testSaveBadFile(self):
+        """ tests wrong type of argument to save_to_file
+        """
         with self.assertRaises(AttributeError):
             Square.save_to_file("a string")
 
     def testFromJSON(self):
+        """ tests loading a json string into a list
+        of dicts
+        """
         d = Square.from_json_string(
             '[{"id": 3, "size": 5, "x": 1, "y": 1}]'
         )
@@ -187,45 +267,66 @@ class TestSquare(unittest.TestCase):
         )
 
     def testFromJSONNonString(self):
+        """ tests a bad type into from_json_string
+        """
         with self.assertRaises(TypeError):
             d = Square.from_json_string(TypeError)
 
     def testFromJSONBadString(self):
+        """ tests a non-json string to list of dicts
+        """
         with self.assertRaises(ValueError):
             d = Square.from_json_string("bad string")
 
     def testCreateSquare(self):
+        """ tests creation of a square
+        """
         d = {"id": 1, "size": 5, "x": 2, "y": 2}
         r = Square.create(**d)
         self.assertEqual(d, r.to_dictionary())
 
     def testCreateBadSquare(self):
+        """ tests creating a square with the wrong
+        attributes
+        """
         d = {"id": 1, "width": 2, "height": 2, "x": 2, "y": -1}
         with self.assertRaises(ValueError):
             r = Square.create(**d)
 
     def testCreateSqrEmptyDict(self):
+        """ tests creating a square from an empty
+        dictionary
+        """
         d = {}
         r = Square.create(**d)
         self.assertEqual(r.id, 2)
 
     def testLoadSqrFromFile(self):
+        """ tests loading a square from file
+        """
         Square.save_to_file([self.sqr])
         r = Square.load_from_file()[0]
         self.assertEqual(
             self.sqr.to_dictionary(), r.to_dictionary())
 
     def testLoadNothingFromFile(self):
+        """ tests loading an empty list from file
+        """
         elist = []
         Square.save_to_file(elist)
         r = Square.load_from_file()
         self.assertEqual(elist, r)
 
     def testLoadSqrNoFile(self):
+        """ tests loading a square if the file
+        does not exist
+        """
         r = Square.load_from_file()
         self.assertEqual(r, [])
 
     def testLoadBadSquare(self):
+        """ tests a bad string loading from a file
+        """
         with open("Square.json", "w") as f:
             f.write("Bad string")
         with self.assertRaises(ValueError):
