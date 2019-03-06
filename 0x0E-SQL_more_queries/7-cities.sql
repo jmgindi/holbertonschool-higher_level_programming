9f234898ee4efa4a2831f6cf4ac9964ef1c6e432
jmgindi <jmgindi@gmail.com>
2019-03-06 01:15:34 +0000

    added use statement

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,7 @@
 -- creates hbtn_0d_usa database and cities table if they don't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
        id INT UNIQUE AUTO_INCREMENT NOT NULL,
        state_id INT NOT NULL,
        name VARCHAR(256) NOT NULL,
