f4e83db42e11a7449027b9d1b0650cb0c26a3c0e
jmgindi <jmgindi@gmail.com>
2019-03-30 05:18:40 +0000

    first upload model city

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module contains 1 class:
+    City
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+
+class City(Base):
+    """City class for use with SQLAlchemy
+    Attributes:
+        __tablename__: table to reference
+        id: id of object instance
+        name: string of max 128 chars not null
+        state_id: foreign key to state
+    """
+
+    __tablename__ = "cities"
+    id = Column(Integer, autoincrement=True,
+                    nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey('states.id'))
