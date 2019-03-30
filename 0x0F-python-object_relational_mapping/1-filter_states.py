d633d026e082dc4a8b72c2f9d3b53250f8a56278
jmgindi <jmgindi@gmail.com>
2019-03-30 04:02:59 +0000

    changed single to triple quotes for line breaks

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states
+    cur.execute("""SELECT * FROM states
                 WHERE name LIKE BINARY 'N%'
-                ORDER BY states.id ASC")
+                ORDER BY states.id ASC""")
 
     states = cur.fetchall()
     for state in states:
