cdad5193ca8636d47f4e40241ac11f96aaf838c7
jmgindi <jmgindi@gmail.com>
2019-05-07 21:42:08 +0000

    fixed semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 if (!process.argv[2] || !process.argv[3]) {
-  console.log(0)
-}
-else {
-  console.log(process.argv.sort().reverse()[1])
+  console.log(0);
+} else {
+  console.log(process.argv.sort().reverse()[1]);
 }
