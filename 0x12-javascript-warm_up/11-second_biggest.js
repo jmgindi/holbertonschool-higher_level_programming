d85dc9abf73d3c166510f76471a1fa1037ecb857
jmgindi <jmgindi@gmail.com>
2019-05-07 21:51:04 +0000

    fixed sort to use numeric value

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,5 +2,6 @@
 if (!process.argv[2] || !process.argv[3]) {
   console.log(0);
 } else {
-  console.log(process.argv.sort().reverse()[1]);
+  
+  console.log(process.argv.sort((a, b) => a - b).reverse()[1]);
 }
