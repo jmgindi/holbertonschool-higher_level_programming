e16777b82282d5a29fc0c3dba359739dd3fbc052
jmgindi <jmgindi@gmail.com>
2019-05-13 19:46:56 +0000

    first commit 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const S = require('./5-square')
+
+module.exports = class Square extends S {
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width))
+    }
+  }
+};
