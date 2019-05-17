2dc56a65a50866241612b6b994fbf485b75b202e
jmgindi <jmgindi@gmail.com>
2019-05-17 02:08:45 +0000

    changed http to https

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,5 +1,5 @@
 $(function () {
-  $.get('http://swapi.co/api/people/5/?format=json', function (data) {
+  $.get('https://swapi.co/api/people/5/?format=json', function (data) {
     $('div#character').text(data.name);
   });
 });
